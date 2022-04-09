b45c13252a4df9be0d7c609c52921db4e661ca7d
Varu <vardaan.agarwal.98@gmail.com>
2022-04-09 17:41:59 +0530

    add: styles and structure

---
+++ src/frontend/ProductPage/ProductPage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ProductCard from "../ProductCard/ProductCard.jsx";
+import ProductFilter from "../ProductFilter/ProductFilter.jsx";
+
+const ProductPage = () => {
+  return (
+    <div className="product-filter-container">
+      <ProductFilter />
+      <div className="product-container">
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </div>
+    </div>
+  );
+};
+
+export default ProductPage;
