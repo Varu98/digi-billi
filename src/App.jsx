c3627feac0a78220a2657ed9e05e7a7013f24c42
Varu <vardaan.agarwal.98@gmail.com>
2022-04-09 17:46:08 +0530

    remove : unused import and home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import "./App.css";
-import Home from "./frontend/Home/Home";
 import Navbar from "./frontend/Home/Navbar/Navbar";
 import ProductPage from "./frontend/ProductPage/ProductPage";
 
@@ -7,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* <Home /> */}
       <ProductPage />
     </div>
   );
