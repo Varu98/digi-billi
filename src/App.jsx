0c0f9d6697e22c577ad518a1a78e837967953e0b
Varu <vardaan.agarwal.98@gmail.com>
2022-04-09 11:42:00 +0530

    add: Homepage styles and structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Home from "./frontend/Home/Home";
+import Navbar from "./frontend/Home/Navbar/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/frontend/Home/Home.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Home.css";
+import { products } from "../../backend/db/products";
+import ProductCard from "../ProductCard/ProductCard.jsx";
+
+const Home = () => {
+  return (
+    <div className="home-wrapper">
+      <div className="category-img-container">
+        <img className="category-img" src="assets/trending.jpg" alt="" />
+        <img className="category-img" src="assets/bestsellers.jpg" alt="" />
+        <img className="category-img" src="assets/latest.jpg" alt="" />
+        <img className="category-img" src="assets/valentine's.jpg" alt="" />
+      </div>
+      {/* Banner Image */}
+      <div className="banner-img">
+        <img src="assets/mobileBanner.jpg" alt="" />
+      </div>
+      <div className="display-products-banner">
+        <span className="display-products-banner__content">
+          Trending Products
+        </span>
+      </div>
+      <div className="display-products-banner__products">
+        <ProductCard />
+      </div>
+
+      <div className="display-products-banner">
+        <span className="display-products-banner__content">
+          Products under 10,000 INR
+        </span>
+      </div>
+      <div className="display-products-banner__products">
+        <ProductCard />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
