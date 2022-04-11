f8df73000e6c8df2e0c2e0d5ed7c610bc50bff28
Varu <vardaan.agarwal.98@gmail.com>
2022-04-11 15:11:10 +0530

    add : Wishlist Screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import "./App.css";
-import CartPage from "./frontend/CartPage/CartPage";
 import Navbar from "./frontend/Home/Navbar/Navbar";
-import ProductPage from "./frontend/ProductPage/ProductPage";
+import WishlistPage from "./frontend/WishlistPage/WishlistPage";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <CartPage />
+      <WishlistPage />
     </div>
   );
 }
