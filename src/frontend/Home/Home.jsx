867939e7b100ec8e57f8e9b5d7acd61ac8f5524a
Varu <vardaan.agarwal.98@gmail.com>
2022-04-18 17:08:21 +0530

    add: Links to banner image and categories

--- src/frontend/Home/Home.jsx
+++ src/frontend/Home/Home.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import "./Home.css";
 import ProductCard from "../ProductCard/ProductCard.jsx";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div className="home-wrapper">
-      <div className="category-img-container">
-        <img className="category-img" src="assets/trending.jpg" alt="" />
-        <img className="category-img" src="assets/bestsellers.jpg" alt="" />
-        <img className="category-img" src="assets/latest.jpg" alt="" />
-        <img className="category-img" src="assets/valentine's.jpg" alt="" />
-      </div>
+      <Link to="/products">
+        <div className="category-img-container">
+          <img className="category-img" src="assets/trending.jpg" alt="" />
+          <img className="category-img" src="assets/bestsellers.jpg" alt="" />
+          <img className="category-img" src="assets/latest.jpg" alt="" />
+          <img className="category-img" src="assets/valentine's.jpg" alt="" />
+        </div>
+      </Link>
       {/* Banner Image */}
       <div className="banner-img">
-        <img src="assets/mobileBanner.jpg" alt="" />
+        <Link to="/products">
+          <img src="assets/mobileBanner.jpg" alt="" />
+        </Link>
       </div>
       <div className="display-products-banner">
         <span className="display-products-banner__content">
