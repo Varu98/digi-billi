c39d6a11112554793d34f73fab5468ba93d37946
Varu <vardaan.agarwal.98@gmail.com>
2022-04-21 23:07:16 +0530

    chore : remove unused import

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Navbar from "./frontend/Home/Navbar/Navbar";
 import Router from "./frontend/Routes/Router";
 
 function App() {
-  const { products, categories } = useProducts();
   return (
     <div className="App">
       <Navbar />
